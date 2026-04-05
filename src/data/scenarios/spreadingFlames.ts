da8265d4c27d4f78912fd778ed801a5e98e9855b
BoomerET <david.berkompas@gmail.com>
2026-04-04 21:42:15 -0500

    Updating Encounter deck

--- src/data/scenarios/spreadingFlames.ts
+++ src/data/scenarios/spreadingFlames.ts
@@ -195,6 +195,29 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       name: "Hellhound",
       type: "enemy",
       ability: ["Hunter"],
+      text: [
+        "Forced - After Hellhound attacks you during the enemy phase: Choose and discard 1 asset you control."
+      ],
+      damage: 1,
+      horror: 1,
+      fight: 2,
+      health: 3,
+      evade: 4,
+      set: "ashen-pilgrims",
+      traits: [
+        "Creature",
+        "Monster"
+      ],
+    },
+    {
+      id: "hellhound-2",
+      code: "12122",
+      name: "Hellhound",
+      type: "enemy",
+      ability: ["Hunter"],
+      text: [
+        "Forced - After Hellhound attacks you during the enemy phase: Choose and discard 1 asset you control."
+      ],
       damage: 1,
       horror: 1,
       fight: 2,
@@ -212,7 +235,49 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       name: "Bystander",
       type: "enemy",
       ability: ["Doomed"],
-      text: ["When this enemy is defeated, place 1 doom on the current agenda. This effect can cause the current agenda to advance"],
+      text: [
+        "When this enemy is defeated, place 1 doom on the current agenda. This effect can cause the current agenda to advance"
+      ],
+      damage: 1,
+      horror: 0,
+      fight: 1,
+      health: 1,
+      evade: 1,
+      set: "bystanders",
+      traits: [
+        "Humanoid",
+        "Civilian"
+      ],
+    },
+    {
+      id: "bystander-2",
+      code: "12123",
+      name: "Bystander",
+      type: "enemy",
+      ability: ["Doomed"],
+      text: [
+        "When this enemy is defeated, place 1 doom on the current agenda. This effect can cause the current agenda to advance"
+      ],
+      damage: 1,
+      horror: 0,
+      fight: 1,
+      health: 1,
+      evade: 1,
+      set: "bystanders",
+      traits: [
+        "Humanoid",
+        "Civilian"
+      ],
+    },
+    {
+      id: "bystander-3",
+      code: "12123",
+      name: "Bystander",
+      type: "enemy",
+      ability: ["Doomed"],
+      text: [
+        "When this enemy is defeated, place 1 doom on the current agenda. This effect can cause the current agenda to advance"
+      ],
       damage: 1,
       horror: 0,
       fight: 1,
@@ -230,7 +295,37 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       name: "Cosmic Evils",
       type: "treachery",
       ability: ["Peril"],
-      text: ["Revelation - You must either (choose one): Place 1 doom on the current agenda. This effect can cause the agenta to advance. OR Take 1 direct damage and 1 direct horror. Cosmic Evils gains surge"],
+      text: [
+        "Revelation - You must either (choose one): Place 1 doom on the current agenda. This effect can cause the agenta to advance. OR Take 1 direct damage and 1 direct horror. Cosmic Evils gains surge"
+      ],
+      set: "cosmic-evils",
+      traits: [
+        "Omen",
+      ],
+    },
+    {
+      id: "cosmic-evils-2",
+      code: "12124",
+      name: "Cosmic Evils",
+      type: "treachery",
+      ability: ["Peril"],
+      text: [
+        "Revelation - You must either (choose one): Place 1 doom on the current agenda. This effect can cause the agenta to advance. OR Take 1 direct damage and 1 direct horror. Cosmic Evils gains surge"
+      ],
+      set: "cosmic-evils",
+      traits: [
+        "Omen",
+      ],
+    },
+    {
+      id: "cosmic-evils-3",
+      code: "12124",
+      name: "Cosmic Evils",
+      type: "treachery",
+      ability: ["Peril"],
+      text: [
+        "Revelation - You must either (choose one): Place 1 doom on the current agenda. This effect can cause the agenta to advance. OR Take 1 direct damage and 1 direct horror. Cosmic Evils gains surge"
+      ],
       set: "cosmic-evils",
       traits: [
         "Omen",
@@ -241,7 +336,26 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       code: "12125",
       name: "Unspeakable Truths",
       type: "treachery",
-      text: ["Revelation - Put Unspeakable Truths into play in your threat area. Limit 1 per investigator.", "Forced - After you discover 1 or more clues: Take 1 Horror", "Spend 2 actions to Discard"],
+      text: [
+        "Revelation - Put Unspeakable Truths into play in your threat area. Limit 1 per investigator.", 
+        "Forced - After you discover 1 or more clues: Take 1 Horror", 
+        "Spend 2 actions to Discard"
+      ],
+      set: "eldritch-lore",
+      traits: [
+        "Terror",
+      ],
+    },
+    {
+      id: "unspeakable-truths-2",
+      code: "12125",
+      name: "Unspeakable Truths",
+      type: "treachery",
+      text: [
+        "Revelation - Put Unspeakable Truths into play in your threat area. Limit 1 per investigator.", 
+        "Forced - After you discover 1 or more clues: Take 1 Horror", 
+        "Spend 2 actions to Discard"
+      ],
       set: "eldritch-lore",
       traits: [
         "Terror",
@@ -252,7 +366,22 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       code: "12126",
       name: "Forbidden Secrets",
       type: "treachery",
-      text: ["Revelation - If you have no clues, Forbidden Secrets gains surge. Otherwise test INT (3). For each point you fail by, you must either place 1 of your clues on your location, or take 1 horror."],
+      text: [
+        "Revelation - If you have no clues, Forbidden Secrets gains surge. Otherwise test INT (3). For each point you fail by, you must either place 1 of your clues on your location, or take 1 horror."
+      ],
+      set: "eldritch-lore",
+      traits: [
+        "Pact",
+      ],
+    },
+    {
+      id: "forbidden-secrets-2",
+      code: "12126",
+      name: "Forbidden Secrets",
+      type: "treachery",
+      text: [
+        "Revelation - If you have no clues, Forbidden Secrets gains surge. Otherwise test INT (3). For each point you fail by, you must either place 1 of your clues on your location, or take 1 horror."
+      ],
       set: "eldritch-lore",
       traits: [
         "Pact",
@@ -263,10 +392,222 @@ export const spreadingFlamesScenario: ScenarioDefinition = {
       code: "12127",
       name: "Extraplanar Vision",
       type: "treachery",
-      text: ["Revelation - Test WIL or INT (X), where X is the number of cards in your hand. If you fail, take 1 damage and discard 1 card at random from your hand."],
-      set: "eldritch-lore",
+      text: [
+        "Revelation - Test WIL or INT (X), where X is the number of cards in your hand. If you fail, take 1 damage and discard 1 card at random from your hand."
+      ],
+      set: "hallucinations",
       traits: [
         "Power",
+      ],
+    },
+    {
+      id: "extraplanar-visions-2",
+      code: "12127",
+      name: "Extraplanar Vision",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL or INT (X), where X is the number of cards in your hand. If you fail, take 1 damage and discard 1 card at random from your hand."
+      ],
+      set: "hallucinations",
+      traits: [
+        "Power",
+      ],
+    },
+    {
+      id: "wild-compulsion-1",
+      code: "12128",
+      name: "Wild Compulsion",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL (3), For each point you fail by, you must either discard 1 card at random from your hand, or lose 1 resource."
+      ],
+      set: "hallucinations",
+      traits: [
+        "Madness",
+        "Bane"
+      ],
+    },
+    {
+      id: "wild-compulsion-2",
+      code: "12128",
+      name: "Wild Compulsion",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL (3), For each point you fail by, you must either discard 1 card at random from your hand, or lose 1 resource."
+      ],
+      set: "hallucinations",
+      traits: [
+        "Madness",
+        "Bane"
+      ],
+    },
+    {
+      id: "fire-1",
+      code: "12129",
+      name: "Fire!",
+      type: "treachery",
+      text: [
+        "Revelation - Attach Fire! to the nearest location without Fire! attached.", 
+        "Forced - When the investigation phase ends: Each non-Elite card with health at this location takes 1 direct damage",
+        "Action: Test SPD (3). If you succeed, discard Fire!",
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "fire-2",
+      code: "12129",
+      name: "Fire!",
+      type: "treachery",
+      text: [
+        "Revelation - Attach Fire! to the nearest location without Fire! attached.", 
+        "Forced - When the investigation phase ends: Each non-Elite card with health at this location takes 1 direct damage",
+        "Action: Test SPD (3). If you succeed, discard Fire!",
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "fire-3",
+      code: "12129",
+      name: "Fire!",
+      type: "treachery",
+      text: [
+        "Revelation - Attach Fire! to the nearest location without Fire! attached.", 
+        "Forced - When the investigation phase ends: Each non-Elite card with health at this location takes 1 direct damage",
+        "Action: Test SPD (3). If you succeed, discard Fire!",
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "fire-4",
+      code: "12129",
+      name: "Fire!",
+      type: "treachery",
+      text: [
+        "Revelation - Attach Fire! to the nearest location without Fire! attached.", 
+        "Forced - When the investigation phase ends: Each non-Elite card with health at this location takes 1 direct damage",
+        "Action: Test SPD (3). If you succeed, discard Fire!",
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "fire-5",
+      code: "12129",
+      name: "Fire!",
+      type: "treachery",
+      text: [
+        "Revelation - Attach Fire! to the nearest location without Fire! attached.", 
+        "Forced - When the investigation phase ends: Each non-Elite card with health at this location takes 1 direct damage",
+        "Action: Test SPD (3). If you succeed, discard Fire!",
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "noxious-smoke-1",
+      code: "12130",
+      name: "Noxious Smoke",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL or SPD (3). Take 1 damage for each point you fail by.", 
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "noxious-smoke-2",
+      code: "12130",
+      name: "Noxious Smoke",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL or SPD (3). Take 1 damage for each point you fail by.", 
+      ],
+      set: "fire",
+      traits: [
+        "Hazard",
+      ],
+    },
+    {
+      id: "mutated-1",
+      code: "12131",
+      name: "Mutated",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL (2). This test gets +2 difficulty if there is an enemy at your location. If you fail, you must either take 2 damage, or each investigator at your location takes 1 horror.", 
+      ],
+      set: "mad-science",
+      traits: [
+        "Mutation",
+      ],
+    },
+    {
+      id: "mutated-2",
+      code: "12131",
+      name: "Mutated",
+      type: "treachery",
+      text: [
+        "Revelation - Test WIL (2). This test gets +2 difficulty if there is an enemy at your location. If you fail, you must either take 2 damage, or each investigator at your location takes 1 horror.", 
+      ],
+      set: "mad-science",
+      traits: [
+        "Mutation",
+      ],
+    },
+    {
+      id: "mutated-experiment-1",
+      code: "12132",
+      name: "Mutated Experiment",
+      type: "enemy",
+      ability: ["Retaliate"],
+      text: [
+        "Forced - AWhen this enemy is defeated: Each investigator at it's location takes 1 horror."
+
+      ],
+      damage: 1,
+      horror: 1,
+      fight: 3,
+      health: 3,
+      evade: 3,
+      set: "mad-science",
+      traits: [
+        "Creature",
+        "Mutated"
+      ],
+    },
+    {
+      id: "mutated-experiment-2",
+      code: "12132",
+      name: "Mutated Experiment",
+      type: "enemy",
+      ability: ["Retaliate"],
+      text: [
+        "Forced - AWhen this enemy is defeated: Each investigator at it's location takes 1 horror."
+
+      ],
+      damage: 1,
+      horror: 1,
+      fight: 3,
+      health: 3,
+      evade: 3,
+      set: "mad-science",
+      traits: [
+        "Creature",
+        "Mutated"
       ],
     },
   ],
