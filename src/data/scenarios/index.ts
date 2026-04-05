9111aba973feb1c40d7d63339b49e2be43a4b6d3
David Berkompas <david.berkompas@gmail.com>
2026-04-05 16:46:24 -0500

    Remove unwanted scenarios
    Core 2026 so far only has Breathren of Ash

--- src/data/scenarios/index.ts
+++ src/data/scenarios/index.ts
@@ -1,7 +1,7 @@
-import { theGatheringScenario } from "./theGathering";
-import { theBeachScenario } from "./theBeach";
+//import { theGatheringScenario } from "./theGathering";
+//import { theBeachScenario } from "./theBeach";
 import { spreadingFlamesScenario } from "./spreadingFlames";
 
-export const scenarios = [theGatheringScenario, theBeachScenario, spreadingFlamesScenario];
-
-export const defaultScenarioId = theGatheringScenario.id;
+//export const scenarios = [theGatheringScenario, theBeachScenario, spreadingFlamesScenario];
+export const scenarios = [spreadingFlamesScenario];
+export const defaultScenarioId = spreadingFlamesScenario.id;
