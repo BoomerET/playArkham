abd5311dd41fc7d96bb68a08e4af389ef8fbeca0
BoomerET <david.berkompas@gmail.com>
2026-04-06 15:41:47 -0500

    Adding another scenario

--- src/data/scenarios/index.ts
+++ src/data/scenarios/index.ts
@@ -1,7 +1,8 @@
 //import { theGatheringScenario } from "./theGathering";
 //import { theBeachScenario } from "./theBeach";
 import { spreadingFlamesScenario } from "./spreadingFlames";
+import { smokeAndMirrorsQuietScenario } from "./smokeAndMirrors";
 
 //export const scenarios = [theGatheringScenario, theBeachScenario, spreadingFlamesScenario];
-export const scenarios = [spreadingFlamesScenario];
+export const scenarios = [spreadingFlamesScenario, smokeAndMirrorsQuietScenario];
 export const defaultScenarioId = spreadingFlamesScenario.id;
