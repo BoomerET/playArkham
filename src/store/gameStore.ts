5299ea275c1c05fbf0a8208657bae79b1f015c39
David Berkompas <david.berkompas@gmail.com>
2026-04-17 15:20:50 -0500

    Update gameStore.ts

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -4318,7 +4318,7 @@ export const useGameStore = create<GameStore>((set, get) => ({
 
   setPhase: (phase) => {
     const { turn, investigator, locations, enemies, campaignState } = get();
-
+    console.log(phase);
     let updatedInvestigator = investigator;
     let updatedLocations = locations;
     let updatedEnemies = enemies;
