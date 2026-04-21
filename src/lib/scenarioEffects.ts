92a1497138d641b245de7142228185c77379d7fc
David Berkompas <david.berkompas@gmail.com>
2026-04-21 10:44:43 -0500

    Update scenarioEffects.ts

--- src/lib/scenarioEffects.ts
+++ src/lib/scenarioEffects.ts
@@ -13,8 +13,8 @@ import type {
 } from "../types/game";
 
 import { findCurrentLocation } from "./gameStateHelpers";
-import { buildEnemyFromEncounterCard, enemyHasAloof, buildLocationAttachmentFromEncounterCard } from "../store/gameStore";
-import { takeSetAsideEncounterCardByCode } from "../store/gsFunctions";
+import { buildEnemyFromEncounterCard, buildLocationAttachmentFromEncounterCard } from "../store/gameStore";
+import { takeSetAsideEncounterCardByCode, enemyHasAloof } from "../store/gsFunctions";
 
 export type ScenarioEffectState = {
   agenda: ScenarioCardState | null;
