c0edb1eea9bb779c56b32f6ccebf76956c646d43
David Berkompas <david.berkompas@gmail.com>
2026-04-21 22:08:43 -0500

    Update gameStore.ts

--- src/store/gameStore.ts
+++ src/store/gameStore.ts
@@ -47,7 +47,7 @@ import {
   isScenarioResolved,
   getScenarioResolvedMessage,
   savePersistedCampaignSetup,
-  buildEnemyFromEncounterCard,
+  spawnEnemyAtLocation,
 } from "./gsFunctions";
 
 import {
