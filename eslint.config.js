53bdb63c0a903a966ba649bf176f915b90ad9f54
David Berkompas <david.berkompas@gmail.com>
2026-05-01 23:53:28 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,8 +7,9 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
-    files: ['**/*.{ts,tsx}', '**/*.test.tsx'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,6 +19,16 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
 ])
