1c433bffbbc50d72c595080e4b6c533f8f3269c4
David Berkompas <david.berkompas@gmail.com>
2026-05-01 23:48:26 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,12 +8,13 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx}', '**/*.test.tsx'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      plugin.vitest/recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
